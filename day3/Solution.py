8f3dc22878f11e478fac92fc3898da32ce02be22
bskowr <105642099+bskowr@users.noreply.github.com>
2022-12-11 16:06:03 +0100

    Finished day 3 task 2

--- day3/Solution.py
+++ day3/Solution.py
@@ -7,6 +7,9 @@ class Rucksack:
         self.first_compartment = contents[:split_point]
         self.second_compartment = contents[split_point:]
 
+    def get_all_contents(self) -> str:
+        return self.first_compartment + self.second_compartment
+
     def find_duplicate(self) -> chr:
         for item in self.first_compartment:
             if item in self.second_compartment:
@@ -27,21 +30,60 @@ class Solution:
         self.input_file: str = input_file
         self.rucksack_list: List[Rucksack] = []
 
+    @staticmethod
+    def find_badge(rucksacks: List[Rucksack]) -> chr:
+        source_rucksack = rucksacks.pop()
+        for item in source_rucksack.get_all_contents():
+            is_badge = True
+            for rucksack in rucksacks:
+                if item not in rucksack.get_all_contents():
+                    is_badge = False
+                    break
+            if is_badge:
+                return item
+        return None
+
+    def make_groups(self) -> List[List[Rucksack]]:
+        groups = []
+        for i in range(0, len(self.rucksack_list) - 2, 3):
+            group = [
+                self.rucksack_list[i],
+                self.rucksack_list[i+1],
+                self.rucksack_list[i+2],
+            ]
+            groups.append(group)
+        return groups
+
+    def find_all_badges(self) -> List[chr]:
+        badge_list = []
+        groups = self.make_groups()
+        for group in groups:
+            badge_list.append(self.find_badge(group))
+        return badge_list
+
     def load_all_rucksacks(self) -> None:
         self.rucksack_list = []
         with open(self.input_file) as data:
             for item_list in data:
                 self.rucksack_list.append(Rucksack(item_list.strip()))
 
-    def calculate_total_priority(self) -> int:
+    @staticmethod
+    def calculate_priority_of_duplicates(rucksack_list: List[Rucksack]) -> int:
         total_priority = 0
-        for rucksack in self.rucksack_list:
+        for rucksack in rucksack_list:
             total_priority += Rucksack.calculate_priority(rucksack.find_duplicate())
+        return total_priority
+
+    @staticmethod
+    def calculate_priority_of_badges(badge_list: List[chr]) -> int:
+        total_priority = 0
+        for badge in badge_list:
+            total_priority += Rucksack.calculate_priority(badge)
         return total_priority
 
     def solve(self) -> dict:
         answers = {}
         self.load_all_rucksacks()
-        answers["Task 1"] = self.calculate_total_priority()
-        answers["Task 2"] = ''
+        answers["Task 1"] = Solution.calculate_priority_of_duplicates(self.rucksack_list)
+        answers["Task 2"] = Solution.calculate_priority_of_badges(self.find_all_badges())
         return answers
